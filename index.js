d75ca1ecc970a4882f2b2e3993f4bde5ec869d90
Sofia <katzmansof@gmail.com>
2023-01-24 14:34:21 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// Code your solutions in this file`
+
+const gifter = ["Charlie", "Samip", "Ali"];
+
+function writeCards(gifter) {
+    const messages = [];
+
+    for (let i = 0; i < gifter.length; i++){
+        messages.push(`Thank you, ${gifter[i]}, for the wonderful surprise gift!`); 
+    } 
+return(messages);
+}
+
+
+
+function countDown(countingInt) {
+var i = countingInt 
+//console.log(`${countingInt}`); 
+while (i > -1 ) {    
+        console.log(i);
+        i --;        
+  } 
+}
+
